78cfff4326fc593a18bf106a751aacc1bebe1f47
Oz1p <72811419+Oz1p@users.noreply.github.com>
2024-02-13 15:51:46 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Stealerium_Decryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(557, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Webhook:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(75, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(557, 156);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Crypt Key:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Salt Bytes:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(75, 198);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(557, 156);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 488);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(620, 100);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 472);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Output:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(75, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(557, 121);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(0, 594);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(96, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Check for Updates";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 610);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stealerium Decryptor v1.0 by Oz1p";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stealerium_Decryptor
+{
+    public partial class Form1 : Form
+    {
+        string version = "1.0";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static byte[] CryptKey;
+        private static byte[] SaltBytes;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.StartsWith("ENCRYPTED:", StringComparison.OrdinalIgnoreCase))
+            {
+                richTextBox3.AppendText($"Webhook: Invalid Webhook" + Environment.NewLine);
+                return;
+            }
+            try
+            {
+                // Get the input text from richTextBox1
+                string inputText = richTextBox1.Text;
+
+                // Split the text into an array of strings based on commas
+                string[] values = inputText.Split(',');
+
+                // Update CryptKey with the parsed byte values
+                CryptKey = new byte[values.Length];
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (byte.TryParse(values[i], out byte parsedValue))
+                    {
+                        CryptKey[i] = parsedValue;
+                    }
+                    else
+                    {
+                        richTextBox3.AppendText($"Crypt Key: Unable to parse value at index {i}: {values[i]}. Please enter valid byte values." + Environment.NewLine);
+                        return; // Exit the method if parsing fails
+                    }
+                }
+            }
+            catch { 
+
+            }
+            try
+            {
+                // Get the input text from richTextBox1
+                string inputText = richTextBox2.Text;
+
+                // Split the text into an array of strings based on commas
+                string[] values = inputText.Split(',');
+
+                // Update CryptKey with the parsed byte values
+                SaltBytes = new byte[values.Length];
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (byte.TryParse(values[i], out byte parsedValue))
+                    {
+                        SaltBytes[i] = parsedValue;
+                    }
+                    else
+                    {
+                        richTextBox3.AppendText($"Salt Bytes: Unable to parse value at index {i}: {values[i]}. Please enter valid byte values." + Environment.NewLine);
+                        return; // Exit the method if parsing fails
+                    }
+                }
+            }
+            catch { 
+            }
+            try
+            {
+                richTextBox3.Clear();
+                richTextBox3.AppendText($"Decrypted Webhook: " + DecryptConfig(textBox1.Text)+ Environment.NewLine);
+                MessageBox.Show("Successfully Decrypted!", "Stealerium Decryptor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                richTextBox3.AppendText($"Decrypt: Error!" + Environment.NewLine);
+            }
+
+
+        }
+        public static string Decrypt(byte[] bytesToBeDecrypted)
+        {
+            byte[] bytes;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+                    Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(CryptKey, SaltBytes, 1000);
+                    aes.Key = rfc2898DeriveBytes.GetBytes(aes.KeySize / 8);
+                    aes.IV = rfc2898DeriveBytes.GetBytes(aes.BlockSize / 8);
+                    aes.Mode = CipherMode.CBC;
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cryptoStream.Close();
+                    }
+                    bytes = memoryStream.ToArray();
+                }
+            }
+            return Encoding.UTF8.GetString(bytes);
+        }
+        public static string DecryptConfig(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return "";
+            }
+            if (!value.StartsWith("ENCRYPTED:"))
+            {
+                return value;
+            }
+            return Decrypt(Convert.FromBase64String(value.Replace("ENCRYPTED:", "")));
+        }
+        static string GetRawContent(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+                    response.EnsureSuccessStatusCode(); // Ensure success status code (2xx)
+
+                    string content = response.Content.ReadAsStringAsync().Result;
+                    return content;
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                    return string.Empty;
+                }
+            }
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://pastebin.com/raw/5MibSPkN";
+
+            string content = GetRawContent(url);
+
+            Console.WriteLine(content);
+            if (content == version)
+            {
+                MessageBox.Show("You are using the lasted version! (" + content + ")", "Stealerium Decryptor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("You are using version " + version + ". There is a newer version available ("+content+")", "Stealerium Decryptor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                System.Diagnostics.Process.Start("https://github.com/Oz1p/Stealerium_Decryptor/tree/main");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string url = "https://pastebin.com/raw/5MibSPkN";
+
+            string content = GetRawContent(url);
+
+            Console.WriteLine(content);
+            if (content == version)
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("You are using version " + version + ". There is a newer version available (" + content + ")", "Stealerium Decryptor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                System.Diagnostics.Process.Start("https://github.com/Oz1p/Stealerium_Decryptor/tree/main");
+            }
+        }
+    }
+    }
+
+
